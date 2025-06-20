17a388eb66fbe27a9dc4ffb4c4d9fedc2e31ccab
argun06 <acg.vision.us@gmail.com>
2025-06-20 03:30:25 +0300

    Update spor_haber_bot_env.py

--- spor_haber_bot_env.py
+++ spor_haber_bot_env.py
@@ -3,22 +3,21 @@ from datetime import datetime
 import time
 from telegram import Bot
 import os
-
-# .env dosyasındaki API anahtarlarını kullanmak için:
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 def haberleri_cek():
-    url = "https://www.ntvspor.net/rss.xml"
+    url = "https://www.fotomac.com.tr/rss/sondakika"
     feed = feedparser.parse(url)
-    bugun = datetime.now().date()
 
+    bugun = datetime.now().date()
     haberler = []
+
     for entry in feed.entries:
         try:
             yayin_tarihi = datetime(*entry.published_parsed[:6]).date()
@@ -26,8 +25,8 @@ def haberleri_cek():
                 haber = {
                     "mac": entry.title,
                     "link": entry.link,
-                    "oran": "Oran bilgisi yok",
-                    "analiz": "AI analizi eklenecek..."
+                    "oran": "Tahmin bilgisi eklenmedi.",
+                    "analiz": "AI yorumları test ediliyor..."
                 }
                 haberler.append(haber)
         except:
